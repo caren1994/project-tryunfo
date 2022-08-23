eb00daf9b49a749692fa5764e0df5bffcc432e6b
caren <carenoliveirasilvapontes@gmail.com>
2022-08-23 18:53:18 -0500

    requisito 5 feito com ajuda do mentor fernando

--- src/App.js
+++ src/App.js
@@ -21,6 +21,24 @@ class App extends React.Component {
     const value = type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => {
+      const { cardName, cardDescription,
+        cardImage, cardRare, cardAttr1, cardAttr2, cardAttr3 } = this.state;
+      const valorDosCards = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+      const numero1 = 90;
+      const numero2 = 210;
+      if (cardName && cardDescription && cardImage && cardRare
+      && cardAttr1 >= 0 && cardAttr1 <= numero1 && cardAttr2 >= 0
+      && cardAttr2 <= numero1 && cardAttr3 >= 0 && cardAttr3 <= numero1
+      && valorDosCards <= numero2) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isSaveButtonDisabled: true,
+        });
+      }
     });
   };
 
